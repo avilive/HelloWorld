0ffe7c8e787d9b960d4fec4ceffac0eee7c80768
A.Ivanyuk <iav@ppl33-35.net>
2021-12-10 11:31:12 +0200

    main.cpp moved up

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm> // for sort() 
+#include <string>
+#include <fstream>
+//#include <ip_filter.h>
+
+int main(int argc, char* argv[])
+{
+    std::cout<<"ASD"<<std::endl;
+    return 0;
+}
